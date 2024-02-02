fde23a86e1095fbb651bb44098aff1a34965651e
kot-annett <annktv@gmail.com>
2024-02-02 21:55:52 +0100

    working with ProfileViewController and SingleImageViewController.

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -11,12 +11,11 @@ final class ProfileViewController: UIViewController {
     
     // MARK: - IB Outlets
     
-    @IBOutlet private weak var userPhoto: UIImageView!
-    @IBOutlet private weak var userNameLabel: UILabel!
-    @IBOutlet private weak var loginNameLabel: UILabel!
-    @IBOutlet private weak var descriptionLabel: UILabel!
-    
-    @IBOutlet private weak var logOutButton: UIButton!
+    private var userPhoto: UIImageView!
+    private var userNameLabel: UILabel!
+    private var loginNameLabel: UILabel!
+    private var descriptionLabel: UILabel!
+    private var logOutButton: UIButton!
     
     // MARK: - UIStatusBarStyle
     
@@ -24,9 +23,64 @@ final class ProfileViewController: UIViewController {
         return .lightContent
     }
     
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let profileImage = UIImage(named: "userPhoto")
+        let userPhoto = UIImageView(image: profileImage)
+        userPhoto.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userPhoto)
+        
+        userPhoto.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        userPhoto.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        userPhoto.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        userPhoto.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        
+        let userNameLabel = UILabel()
+        userNameLabel.text = "Екатерина Новикова"
+        userNameLabel.textColor = .white
+        userNameLabel.font = UIFont.boldSystemFont(ofSize: 23.0)
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userNameLabel)
+        userNameLabel.leadingAnchor.constraint(equalTo: userPhoto.leadingAnchor).isActive = true
+        userNameLabel.topAnchor.constraint(equalTo: userPhoto.bottomAnchor, constant: 8).isActive = true
+        
+        let loginNameLabel = UILabel()
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.textColor = .gray
+        loginNameLabel.font = UIFont.boldSystemFont(ofSize: 13)
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginNameLabel)
+        loginNameLabel.leadingAnchor.constraint(equalTo: userPhoto.leadingAnchor).isActive = true
+        loginNameLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello world!"
+        descriptionLabel.textColor = .white
+        descriptionLabel.font = UIFont.boldSystemFont(ofSize: 13)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        descriptionLabel.leadingAnchor.constraint(equalTo: userPhoto.leadingAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        let logOutButton = UIButton.systemButton(
+            with: UIImage(named: "exitButton")!,
+            target: self,
+            action: #selector(Self.logOutButtonTapped)
+        )
+        logOutButton.tintColor = UIColor(red: 245, green: 107, blue: 108, alpha: 1)
+        logOutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logOutButton)
+        logOutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        logOutButton.centerYAnchor.constraint(equalTo: userPhoto.centerYAnchor).isActive = true
+        
+    }
+    
     // MARK: - IB Actions
     
-    @IBAction private func logOutButtonTapped(_ sender: UIButton) {
+    @IBAction private func logOutButtonTapped() {
     }
     
     // MARK: - Public Methods
--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -12,18 +12,59 @@ final class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             singleImage.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var singleImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
         singleImage.image = image
+        rescaleAndCenterImageInScrollView(image: image)  
     }
     
     @IBAction func backButtonDidTap(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func shareButtonDidTap(_ sender: Any) {
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+    
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let horizontalScale = visibleRectSize.width / imageSize.width
+        let vetricalScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(horizontalScale, vetricalScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+        
+//        let x = max(0, (scrollView.bounds.width - newContentSize.width) / 2)
+//        let y = max(0, (scrollView.bounds.height - newContentSize.height) / 2)
+//        scrollView.contentInset = UIEdgeInsets(top: y, left: x, bottom: y, right: x)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        singleImage
     }
 }
