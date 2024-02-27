fc40d79a2a03ae12dd2487fe232a6f9731f93444
kot-annett <annktv@gmail.com>
2024-02-27 21:00:57 +0200

    working with KeychainWrapper in OAuth2TokenStorage

--- ImageFeed/ServiceAccessLayer/OAuth2/OAuth2TokenStorage.swift
+++ ImageFeed/ServiceAccessLayer/OAuth2/OAuth2TokenStorage.swift
@@ -6,16 +6,23 @@
 //
 
 import Foundation
+import SwiftKeychainWrapper
 
 final class OAuth2TokenStorage {
     private let tokenKey = "OAuth2AccessToken"
     
     var token: String? {
         get {
-            return UserDefaults.standard.string(forKey: tokenKey)
+            //return UserDefaults.standard.string(forKey: tokenKey)
+            return KeychainWrapper.standard.string(forKey: tokenKey)
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: tokenKey)
+            //UserDefaults.standard.set(newValue, forKey: tokenKey)
+            if let newValue = newValue {
+                KeychainWrapper.standard.set(newValue, forKey: tokenKey)
+            } else {
+                KeychainWrapper.standard.removeObject(forKey: tokenKey)
+            }
         }
     }
 }
